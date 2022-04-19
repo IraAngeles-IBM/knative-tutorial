a126c371aa95cbf34bd58c80f1ab182bf6e4aff0
IraAngeles-IBM <isaias@sg.ibm.com>
2022-04-19 12:30:14 +0800

    update client to 6

--- serving/grpc/csharp/GrpcGreeterClient/Program.cs
+++ serving/grpc/csharp/GrpcGreeterClient/Program.cs
@@ -14,8 +14,12 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
-using GrpcGreeter;
+// using GrpcGreeterClient;
+// using Grpc.Net.Client;
+using GrpcGreeterClient;
 using Grpc.Net.Client;
+
+
 
 namespace GrpcGreeterClient
 {
@@ -23,20 +27,30 @@ namespace GrpcGreeterClient
     {
         static async Task Main(string[] args)
         {
-            AppContext.SetSwitch(
-                "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",
-                true);
-            var httpClient = new HttpClient();
-            // The port number(50051) must match the port of the gRPC server.
-            httpClient.BaseAddress = new Uri("http://application-64.myum6f5wtpf.svc.cluster.local");
-            // httpClient.BaseAddress = new Uri("http://localhost:50051");
-            //httpClient.BaseAddress = new Uri("http://grpc-greeter.default.35.205.12.104.nip.io");
-            var client = GrpcClient.Create<Greeter.GreeterClient>(httpClient);
+            // AppContext.SetSwitch(
+            //     "System.Net.Http.SocketsHttpHandler.Http2UnencryptedSupport",
+            //     true);
+            // var httpClient = new HttpClient();
+            // // The port number(50051) must match the port of the gRPC server.
+            // httpClient.BaseAddress = new Uri("http://application-64.myum6f5wtpf.svc.cluster.local");
+            // // httpClient.BaseAddress = new Uri("http://localhost:50051");
+            // //httpClient.BaseAddress = new Uri("http://grpc-greeter.default.35.205.12.104.nip.io");
+            // var client = GrpcClient.Create<Greeter.GreeterClient>(httpClient);
+            // var reply = await client.SayHelloAsync(
+            //                   new HelloRequest { Name = "GreeterClient" });
+            // Console.WriteLine("Greeting: " + reply.Message);
+            // Console.WriteLine("Press any key to exit...");
+            // Console.ReadKey();
+            //====================
+            // The port number must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("http://application-64.myum6f5wtpf.svc.cluster.local:7042");
+            var client = new Greeter.GreeterClient(channel);
             var reply = await client.SayHelloAsync(
-                              new HelloRequest { Name = "GreeterClient" });
+                            new HelloRequest { Name = "GreeterClient" });
             Console.WriteLine("Greeting: " + reply.Message);
             Console.WriteLine("Press any key to exit...");
             Console.ReadKey();
+
         }
     }
 }
