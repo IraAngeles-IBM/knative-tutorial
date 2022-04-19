9b614d5bbc4a653780304452aee97f574facaff3
IraAngeles-IBM <isaias@sg.ibm.com>
2022-04-19 13:34:41 +0800

    update grpc client version

--- serving/grpc/csharp/GrpcGreeterClient/Program.cs
+++ serving/grpc/csharp/GrpcGreeterClient/Program.cs
@@ -28,7 +28,7 @@ namespace GrpcGreeterClient
                 true);
             var httpClient = new HttpClient();
             // The port number(50051) must match the port of the gRPC server.
-            httpClient.BaseAddress = new Uri("http://localhost:50051");
+            httpClient.BaseAddress = new Uri("http://application-64.myum6f5wtpf.svc.cluster.local:50051");
             //httpClient.BaseAddress = new Uri("http://grpc-greeter.default.35.205.12.104.nip.io");
             var client = GrpcClient.Create<Greeter.GreeterClient>(httpClient);
             var reply = await client.SayHelloAsync(
